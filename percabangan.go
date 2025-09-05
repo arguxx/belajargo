230d6f10d273c97fb5924ad07a42bcf79b52ff7f
arguxx <argianradityaputra21@gmail.com>
2025-09-05 11:16:33 +0700

    add percabangan

--- percabangan.go
+++ percabangan.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	name := "Budi"
+	name := "Budi kuda lari gagah berani hehe"
 
 	if name == "Argian Raditya" {
 		fmt.Println("Hello, Argian Raditya!")
@@ -12,5 +12,11 @@ func main() {
 		fmt.Println("ini argu?")
 	} else {
 		fmt.Println("Hi, siapa kamu?")
+	}
+
+	if length := len(name); length > 5 {
+		fmt.Println("Nama terlalu panjang")
+	} else {
+		fmt.Println("Nama sudah benar")
 	}
 }
